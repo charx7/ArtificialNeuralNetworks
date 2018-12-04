ed203791693b33201ebc0c4c9a451c6782d2ff4d
Anton Laukemper <antonlaukemper@yahoo.de>
2018-12-04 19:23:23 +0100

    added a function that returns the accuracy for a given weight vector. Also plotted the results

--- assignment1.py
+++ assignment1.py
@@ -3,15 +3,30 @@ from testDataGeneration import plotData
 import numpy as np
 from matplotlib import pyplot as plt
 # X datapoints in Y dimenions
-data, weights = generateRandomData(3,2)
-epochs = 3 # Number of epochs
+data, weights = generateRandomData(10,2)
+epochs = 10 # Number of epochs
 learnRate = 1 / 2
+
+def calculateError(weights, data):
+    errors = 0
+    numberOfPoints = data.shape[0]
+    for iter in range(numberOfPoints):
+        currentPoint = data[iter][0:2].reshape(1, 2)
+        currentLabel = data[iter][2]
+        localPotential = np.dot(weights.flatten(), currentPoint.flatten()) * currentLabel
+        if np.sign(localPotential)!=np.sign(currentLabel):
+            errors = errors + 1
+
+    return 1-errors/numberOfPoints
+
+accuracies = []
+
 for i in range(epochs):
     numberOfPoints = data.shape[0]
     for iter in range(numberOfPoints):
         print("epoch " + str(i))
         print("iteration " + str(iter))
-        print("weights " + str(weights))
+        #print("weights " + str(weights))
         # Get the current point and reshape it to be able to use the dot-product
         currentPoint = data[iter][0:2].reshape(1, 2)
         currentLabel = data[iter][2]
@@ -22,7 +37,7 @@ for i in range(epochs):
         else:
             # Assign the weights as the dot product
             localPotential = np.dot(weights.flatten(), currentPoint.flatten()) * currentLabel
-            print("local Potential "+ str(localPotential))
+            # print("local Potential "+ str(localPotential))
             # Since the cutoff point is zero we check whether the local potential is smaller or equal than 0
             # only then the hebbian term becomes 1 and the weights are updated
             if localPotential > 0:
@@ -44,4 +59,12 @@ for i in range(epochs):
     plt.plot(plotWeights.flatten() * 10)
     plt.title(i)
     plt.show()
+
+    accuracies.append(calculateError(weights, data))
+
+print(accuracies)
+plt.figure()
+plt.plot([i for i in range(0,epochs)], accuracies)
+plt.title("accuracy over time")
+plt.show()
 
