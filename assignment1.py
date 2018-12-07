804c49132076cd22bc6d86d73e230e58281e70ad
Carlos Huerta <carlos.huertaso@udlap.mx>
2018-12-07 11:43:01 +0100

    rosenblatz changes

--- assignment1.py
+++ assignment1.py
@@ -2,10 +2,6 @@ from generateRandomData import generateRandomData
 from testDataGeneration import plotData
 import numpy as np
 from matplotlib import pyplot as plt
-# X datapoints in Y dimenions
-data, weights = generateRandomData(10,2)
-epochs = 10 # Number of epochs
-learnRate = 1 / 2
 
 def calculateError(weights, data):
     errors = 0
@@ -13,58 +9,87 @@ def calculateError(weights, data):
     for iter in range(numberOfPoints):
         currentPoint = data[iter][0:2].reshape(1, 2)
         currentLabel = data[iter][2]
-        localPotential = np.dot(weights.flatten(), currentPoint.flatten()) * currentLabel
+        localPotential = np.dot(weights.flatten(), currentPoint.flatten())
         if np.sign(localPotential)!=np.sign(currentLabel):
             errors = errors + 1
 
     return 1-errors/numberOfPoints
 
-accuracies = []
+def prettyPlot(x, weights):
+    # Declare a figure
+    fig = plt.figure()
+    # Plot the scatter
+    my_colors = {'1.0' : 'red','-1.0' : 'green'}
+    c = [my_colors[str(val)] for val in data[:,2]]
+    plt.scatter(data[:, 0], data[:, 1], color=c)
 
-for i in range(epochs):
-    numberOfPoints = data.shape[0]
-    for iter in range(numberOfPoints):
+    # Draw the classifier line
+    ax = plt.axes()
+    classifier = lambda x: -(x * weights[0][0]) / weights[0][1]
+    vfunc = np.vectorize(classifier)
+    y = vfunc(x)
+
+    # Display plot :D
+    ax.plot(x, y);
+
+if __name__ == '__main__':
+    # X datapoints in Y dimenions
+    data, weights = generateRandomData(10,2)
+    # Plot you dataz
+    #plotData(data)
+
+    # min and max of generated data
+    min = np.amin(data)
+    max = np.amax(data)
+    # Declare a linspace for the data generated
+    x = np.linspace(min, max, 20)
+
+    epochs = 2 # Number of epochs
+    # Get the dimensions
+    dims = data.shape[1] - 1
+    # Set the learn rate according to the dims
+    learnRate = 1 / dims
+
+    # Ones vector generation
+    onesVector = np.ones(dims)
+    accuracies = []
+    for i in range(epochs):
+        # Get the number of points for the inner loop
+        numberOfPoints = data.shape[0]
+
         print("epoch " + str(i))
-        print("iteration " + str(iter))
-        #print("weights " + str(weights))
-        # Get the current point and reshape it to be able to use the dot-product
-        currentPoint = data[iter][0:2].reshape(1, 2)
-        currentLabel = data[iter][2]
+        for iter in range(numberOfPoints):
+            # Get the current point and reshape it to be able to use the dot-product
+            currentPoint = data[iter][0:2].reshape(1, 2)
+            print(currentPoint)
+            currentLabel = data[iter][2]
 
-        if iter == 0 and i == 0:
-            # initially assign the weights as the first point
-            weights = (np.multiply([[1,1]], currentPoint) * data[iter][2] ) * learnRate
-        else:
-            # Assign the weights as the dot product
-            localPotential = np.dot(weights.flatten(), currentPoint.flatten()) * currentLabel
-            # print("local Potential "+ str(localPotential))
-            # Since the cutoff point is zero we check whether the local potential is smaller or equal than 0
-            # only then the hebbian term becomes 1 and the weights are updated
-            if localPotential > 0:
-                weights = weights # hebbian = 0
+            if iter == 0 and i == 0:
+                # initially assign the weights as the first point
+                weights = (np.multiply(onesVector, currentPoint) * currentLabel) * learnRate
             else:
-                weights = weights + learnRate * currentPoint * currentLabel
+                # Assign the weights as the dot product
+                localPotential = np.multiply(np.dot(weights.flatten(), currentPoint.flatten()), currentLabel)
+                # Since the cutoff point is zero we check whether the local potential is smaller or equal than 0
+                # only then the hebbian term becomes 1 and the weights are updated
+                if localPotential > 0:
+                    weights = weights # hebbian = 0
+                else:
+                    weights = np.add(weights, np.multiply(learnRate * currentPoint, currentLabel))
+                print('Current weights are:\n ',weights)
+                print('end')
+            #prettyPlot(x, weights)
+        # Print decision boundry for viz
+        #if dims == 2:
+        #    prettyPlot(x, weights)
 
-            #updating the weights
-            # weights = weights + learnRate * hebbian * currentPoint * currentLabel
+        # Append accuracies
+        accuracies.append(calculateError(weights, data))
 
-        # print(weights)
-
+    print(accuracies)
     plt.figure()
-    plotData(data)
-    plotWeights = weights
-    # print(plotWeights)
-    # plotWeights[0][0] = weights[0][1]
-    # plotWeights[0][1] = - weights[0][0]
-    plt.plot(plotWeights.flatten() * 10)
-    plt.title(i)
+    plt.plot([i for i in range(0,epochs)], accuracies)
+    plt.title("accuracy over time")
     plt.show()
 
-    accuracies.append(calculateError(weights, data))
-
-print(accuracies)
-plt.figure()
-plt.plot([i for i in range(0,epochs)], accuracies)
-plt.title("accuracy over time")
-plt.show()
-
+    print('The final weights are: ', weights)
