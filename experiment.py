521f9a640e4b732bb2a8d98875220432eba7c2d2
Carlos Huerta <carlos.huertaso@udlap.mx>
2018-12-11 12:18:39 +0100

    Label name for the current number of dimensions

--- experiment.py
+++ experiment.py
@@ -15,13 +15,13 @@ from tqdm import tqdm
 # pick an Alpha, that determines the ratio of P/N
 # determine N (number of dimensions) as P/Alpha
 # create n_d independent datasets wit P points and N dimensions
-n_d = 50
+n_d = 100
 # let the perceptron run and check whether it converges
 # increase Alpha (alpha =0.75,1.0,1.25,...3.0) and obtain Q (the number of successful runs
 # as a function of alpha)
 
-N_values = [20,  50, 70]
-alphas = np.linspace(0.5, 3, 20)
+N_values = [20, 50, 80, 110, 140]
+alphas = np.linspace(0.5, 3, 40)
 
 # each row is for one P_value (so we do multiple experiments)
 # so row 1 will be the y values for P = 20 and the x values are the alpha values
@@ -29,11 +29,11 @@ alphas = np.linspace(0.5, 3, 20)
 Q = np.zeros([len(N_values),len(alphas)])
 
 for i, N in enumerate(N_values):
-    print('Computing the l.s. of: ', N, ' dimensional space...\n')
+    print('\nComputing the l.s. of: ', N, ' dimensional space...\n')
     for j, alpha in enumerate(tqdm(alphas)):
         P = int(np.round(alpha * N))   # int(np.round(P/alpha))
         success_counter = 0
-        epochs = 100
+        epochs = 200
         learning_rate = 1.0/N #Number of dimensions
         for trial in range(0, n_d):
             data, weights = generateRandomData(P, N)
@@ -43,9 +43,10 @@ for i, N in enumerate(N_values):
 
 plt.figure()
 for i in range(0,len(N_values)):
-
-    plt.plot(alphas, Q[i])
+    currentLabel = str(N_values[i]) + ' dimensions'
+    plt.plot(alphas, Q[i], label = currentLabel)
     plt.title("Separation probability based on alpha")
     plt.xlabel("alpha = P/N")
     plt.ylabel("probability of separation")
+plt.legend()
 plt.show()
